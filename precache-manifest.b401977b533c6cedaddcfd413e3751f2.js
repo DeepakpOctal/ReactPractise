bb93f04ddae1302785fbf74b170f1659490869c5
DeepakpOctal <deepakp@gmail.com>
2020-05-13 20:07:38 +0530

    Updates

---
+++ precache-manifest.b401977b533c6cedaddcfd413e3751f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b374b80cae01a17c90a1d8419ea8b366",
+    "url": "/ReactPractise/index.html"
+  },
+  {
+    "revision": "418a00301f6eb320e7da",
+    "url": "/ReactPractise/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "931144758348a078ef80",
+    "url": "/ReactPractise/static/js/2.eb674b0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPractise/static/js/2.eb674b0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418a00301f6eb320e7da",
+    "url": "/ReactPractise/static/js/main.c3cbf85f.chunk.js"
+  },
+  {
+    "revision": "916348df513a00f1044f",
+    "url": "/ReactPractise/static/js/runtime-main.5b3b96a8.js"
+  }
+]);
