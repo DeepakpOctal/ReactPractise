a8a3ce5956c75d6766e6a1457f1cd3df87951e7f
DeepakpOctal <deepakp@gmail.com>
2020-05-14 17:42:39 +0530

    make changes for varify gitpages

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Footer(){
+
+    return(
+
+        <p>hi there i am deepak mukhraj patel <br />
+           i am just practising the react app for my further requirements
+        </p>
+    )
+}
+
+
+export default Footer
